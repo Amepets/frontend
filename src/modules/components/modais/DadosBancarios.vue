15470e6f6d35ed819e46da026e7f0687ca21200e
bernardo.nunes <bernardo.nunes@cedrotech.com>
2020-11-29 23:24:40 -0300

    ajustes gerais

--- src/modules/components/modais/DadosBancarios.vue
+++ src/modules/components/modais/DadosBancarios.vue
@@ -1,15 +1,21 @@
 <template>
   <v-dialog v-model="showModal" persistent max-width="740px">
-    <v-card class="py-6 px-4" style="border-radius:20px">
+    <v-card class="py-6 px-4" style="border-radius: 20px">
       <div>
         <v-flex class="d-flex align-start justify-space-between flex-row mb-4">
-          <h1 class="font-weight-bold" style="font-size:40px">
+          <h1 class="font-weight-bold" style="font-size: 40px">
             Dados bancários
           </h1>
           <v-icon size="30px" @click="toggleModal">mdi-close</v-icon>
         </v-flex>
         <v-flex class="d-flex justify-space-between flex-wrap">
-          <div class="dados d-flex align-center mb-3">
+          <div
+            :class="
+              this.$vuetify.breakpoint.width < 768
+                ? 'dados d-flex align-center mb-3'
+                : 'dados d-flex align-center'
+            "
+          >
             <div>
               <img
                 class="mr-3"
@@ -18,16 +24,16 @@
               />
             </div>
             <div>
-              <p class="font-weight-bold" style="color:#4f4e50">
+              <p class="font-weight-bold" style="color: #4f4e50">
                 Banco do Brasil
               </p>
-              <p class="font-weight-bold" style="color:#4f4e50">
+              <p class="font-weight-bold" style="color: #4f4e50">
                 CNPJ: 01.181.582/0001-12
               </p>
-              <p class="font-weight-bold" style="color:#4f4e50">
+              <p class="font-weight-bold" style="color: #4f4e50">
                 Agência: 0098-1
               </p>
-              <p class="font-weight-bold" style="color:#4f4e50">
+              <p class="font-weight-bold" style="color: #4f4e50">
                 Conta Corrente: 123584-2
               </p>
             </div>
@@ -41,24 +47,28 @@
               />
             </div>
             <div>
-              <p class="font-weight-bold" style="color:#4f4e50">
+              <p class="font-weight-bold" style="color: #4f4e50">
                 Caixa Econômica Federal
               </p>
-              <p class="font-weight-bold" style="color:#4f4e50">
+              <p class="font-weight-bold" style="color: #4f4e50">
                 Agência: 1910-1
               </p>
-              <p class="font-weight-bold" style="color:#4f4e50">
+              <p class="font-weight-bold" style="color: #4f4e50">
                 Conta Corrente: 01696-3
               </p>
-              <p class="font-weight-bold" style="color:#4f4e50">Operação: 03</p>
+              <p class="font-weight-bold" style="color: #4f4e50">
+                Operação: 03
+              </p>
             </div>
           </div>
         </v-flex>
-        <img
-          class="mt-6"
-          width="100%"
-          src="../../../assets/imgs/img-dados-bancarios.png"
-        />
+        <div class="d-flex justify-center">
+          <img
+            class="mt-6"
+            width="50%"
+            src="../../../assets/imgs/img-dados-bancarios.png"
+          />
+        </div>
       </div>
     </v-card>
   </v-dialog>
